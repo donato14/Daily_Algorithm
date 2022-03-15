6cff64f99f6ae17a2e583ca88c427283b8db1888
Kang Kujun <donato14@naver.com>
2022-03-15 18:19:34 +0900

    3/15 Studying

---
+++ beakjoon/3/15.py
@@ -0,0 +1,18 @@
+#1316
+# sentence = int(input())
+# result = sentence
+# for i in range(sentence):
+#     word=input()
+#     for j in range(len(word)-1):
+#         if word[j]==word[j+1]:
+#             pass
+#         elif word[j] in word[j+1:]:
+#             result-=1
+#             break
+# print(result)
+
+#10757
+# a,b = map(int, input().split())
+
+# print(a+b)
+
