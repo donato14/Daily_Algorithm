6b0f8e4753a3133582ad2732e1c517763c575b25
Kang Kujun <donato14@naver.com>
2022-12-29 23:14:08 +0900

    JS_Algorithm_6

---
+++ JavaScript/6.홀수.js
@@ -0,0 +1,24 @@
+function solution(arr){
+  let answer=[];
+  let temp = [];
+  let min = 0;
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] % 2 != 0) {
+      temp.push(arr[i]);
+      sum += arr[i]
+    } 
+  }
+  temp = temp.sort(function (a, b) {
+    return a - b;
+  });
+  min = temp[0];
+  
+  answer.push(sum);
+  answer.push(min);
+
+  return answer;
+}
+
+arr=[12, 77, 38, 41, 53, 92, 85];
+console.log(solution(arr));
