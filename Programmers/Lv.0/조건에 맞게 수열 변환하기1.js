b777d96cb41d97cbd543359fc365d71bb9e184a1
kangkujun <donato14@naver.com>
2023-05-02 14:28:02 +0900

    📝 Add Lv.0 5 answers

---
+++ Programmers/Lv.0/조건에 맞게 수열 변환하기1.js
@@ -0,0 +1,19 @@
+function solution(arr) {
+  var answer = [];
+  
+  for(let i = 0; i < arr.length; i++){
+      if(arr[i] >= 50 ){
+          if(arr[i] % 2 == 0){
+              arr[i] = arr[i] / 2;    
+          }
+          
+      }else if (arr[i] < 50){
+          if(arr[i] % 2 == 1){
+              arr[i] = arr[i] * 2;    
+          }
+          
+      }
+  }
+  answer = arr;    
+  return answer;
+}
