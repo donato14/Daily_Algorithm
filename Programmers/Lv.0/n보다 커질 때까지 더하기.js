edc401946c327a035db3a1186b13c0510c6832ac
Kang Kujun <donato14@naver.com>
2023-05-10 23:12:05 +0900

    📝 Add Lv.0 2 answers

---
+++ Programmers/Lv.0/n보다 커질 때까지 더하기.js
@@ -0,0 +1,13 @@
+function solution(numbers, n) {
+    var answer = 0;
+    
+    for(let i = 0; i < numbers.length; i++){
+        if(answer <= n){
+            answer = answer + numbers[i];
+        }else{
+            break;
+        }
+    }
+    
+    return answer;
+}
